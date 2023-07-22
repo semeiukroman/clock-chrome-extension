78fcdc83c0c0fc914e3c1dadbc6c6734895f281e
semeniukroman <romansemeniuk635@gmail.com>
2023-07-22 02:21:09 +0200

    first raw version of clock with no features

---
+++ script.js
@@ -0,0 +1,30 @@
+const clock = document.createElement('div');
+clock.classList.add('clock_extension', 'hide');
+setInterval(updateClock, 1000);
+updateClock();
+document.body.append(clock);
+
+chrome.storage.local.get(['display'], result => {
+    if (result.display) clock.classList.remove('hide');
+});
+
+chrome.storage.onChanged.addListener(function (changes, namespace) {
+    clock.classList.toggle('hide');
+});
+
+document.addEventListener('keydown', function (event) {
+    if (event.code === 'Enter') {
+        chrome.storage.local.set({ display: clock.classList.contains('hide') });
+        // clock.classList.toggle('hide');
+    }
+})
+
+function updateClock() {
+    const date = new Date();
+    const time = new Intl.DateTimeFormat('en-En', {
+        hour: 'numeric',
+        minute: 'numeric',
+        second: 'numeric'
+    }).format(date);
+    clock.innerText = time;
+}
